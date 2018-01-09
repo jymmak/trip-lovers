a624bb4a09fafc45988e6d96b4472fde675066aa
Gabriel Loayza Mesias <loayza.lourdes@gmail.com>
2018-01-08 23:35:00 -0500

    cambios vistas y carga de data friends

---
+++ js/search-friends.js
@@ -0,0 +1,25 @@
+$(document).ready(begin);
+function begin() {
+  function getFriends() {
+    return socialNetData;
+  }
+  var dataFriends = getFriends();
+  var arrFriends = dataFriends.AMIGOS;
+  var $boxFriends = $('#cointainer-friends');
+  var $boxFriendsDetails = $('#cointainer-friends');
+  function showFriends() {
+    var str = '';
+    for (var i = 0; i < arrFriends.length; i++) {
+      var place = '<div class="card mb-2">' +
+        '<a href="#"> <img class="card-img-top img-fluid w-100" src="' + arrFriends[i].photo + '" alt="' + arrFriends[i].name + '"></a>' +
+        '<div class="card-body d-flex justify-content-between">' +
+        '<h6 class="card-title mb-1 d-inline">' +
+        '<a href="#">' + arrFriends[i].name + 
+        '</a></h6></div></div>';
+      str += place;
+    }
+    $boxFriends.html(str);
+  }
+
+  showFriends();
+}
---
+++ js/social-net-data.js
@@ -0,0 +1,61 @@
+var socialNetData = {
+  'AMIGOS': [
+    {
+      'code': '1',
+      'name': 'Sara Segovia',
+      'email': 'sara.segovia@gmail.com',
+      'phone': '+51 1 998 723 649',
+      'photo': 'https://picsum.photos/700/450?image=996'
+    },
+    {
+      'code': '2',
+      'name': 'José Lescano',
+      'email': 'leski@hotmail.com',
+      'phone': '+51 1 967 678 345',
+      'photo': 'https://picsum.photos/700/450?image=1001'
+    },
+    {
+      'code': '3',
+      'name': 'Liliana Sandoval',
+      'email': '123lily@gmail.com',
+      'phone': '+51 1 976 345 239',
+      'photo': 'https://picsum.photos/700/450?image=839'
+    },
+    {
+      'code': '4',
+      'name': 'Jean Daza',
+      'email': 'daza.jean@yahoo.com',
+      'phone': '+51 1 967 562 087',
+      'photo': 'https://picsum.photos/700/450?image=1012'
+    },
+    {
+      'code': '5',
+      'name': 'Michael Parraga',
+      'email': 'parra7829@gmail.com',
+      'phone': '+51 1 972 975 002',
+      'photo': 'https://picsum.photos/700/450?image=1005'
+    },
+    {
+      'code': '6',
+      'name': 'Matilde Velazques',
+      'email': 'matyla253@hotmail.com',
+      'phone': '+51 1 990 962 476',
+      'photo': 'https://picsum.photos/700/450?image=838'
+    },
+    {
+      'code': '7',
+      'name': 'Jorge Higa',
+      'email': 'higa.ksh23@gmail.com',
+      'phone': '+51 1 986 452 038',
+      'photo': 'https://picsum.photos/700/450?image=883'
+    },
+    {
+      'code': '8',
+      'name': 'Andrea Garcia',
+      'email': 'andrela243@gmail.com',
+      'phone': '+51 1 976 356 002',
+      'photo': 'https://picsum.photos/700/450?image=823'
+    }    
+  ]
+};
+  
