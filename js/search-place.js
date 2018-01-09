11048b5f07260ce25bbc062e190f169324209837
mebipeos <mebipeos@outlook.com>
2018-01-09 11:44:44 -0500

    modificando comentario

--- js/search-place.js
+++ js/search-place.js
@@ -1,13 +1,18 @@
 function begin() {
-  // console.log('primero');
+  // Obtiene el objeto de los lugares
   var dataPlace = getPlace();
+  // Almacena los nombres de los lugares en un arreglo
   var arrPlace = Object.keys(dataPlace);
+  // Caja donde se va cargar todos los lugares
   var $boxPlace = $('.card-columns');
+  // Extrae la data
   function getPlace() {
     return tripData;
   }
+  // Carga todos los lugares
   function showPlaces() {
     var listPlaces = '';
+    // recorrido de cada uno de los lugares que se almacenó
     for (var i = 0; i < arrPlace.length; i++) {
       var cardPlace = '<div class="card">' +
         '<a href="#"> <img class="card-img-top" src="' + dataPlace[arrPlace[i]]['photo'] + '" alt="' + arrPlace[i] + '"></a>' +
@@ -22,7 +27,7 @@ function begin() {
     $boxPlace.html(listPlaces);
   }
 
-  showPlaces();
+  // Mostrar los detalles de los lugares que se va seleccionar
   function showDetailsPlace() {
     var detailsPlace = dataPlace[$(this).text()];
     if (detailsPlace) {
@@ -31,10 +36,11 @@ function begin() {
     }
   }
 
+  // Añadiendo el comentario del textarea
   function addComentTextArea() {
     var user = 'User';
     var text = $('#text-coment').val();
-    var coment = '<a class="list-group-item list-group-item-action" href="#">'
+    var coment = '<a class="list-group-item list-group-item-action" href="#">' +
     '<div class="media">' +
     '<img class="d-flex mr-3 rounded-circle" src="http://placehold.it/45x45" alt="">' +
     '<div class="media-body">' +
@@ -43,89 +49,55 @@ function begin() {
       '</div>' +
       '</div>' +
       '</a>';
-    // $('#form-comment').hide();
-    // $('.add-coment').show();
-    var $padre = $(this).parent();
-    // $padre.prev().insertAfter(coment);
-    $('.add-coment').insertAfter(coment);
+    $('.add-coment').prev().after(coment);
+    $('#form-comment').hide();
+    $('.add-coment').show();
   }
 
-  function addComent() {
+
+  function addComentForm() {
     var boxComent = '<form id="form-comment" class="container"><div class="form-group">' +
       '<textarea class="form-control" id="text-coment"' +
       ' rows="2" placeholder="Añadir comentario"></textarea></div>' +
       '<button class="btn btn-primary btn-sm btn-check"><i class="fa fa-check"></i></button><button type="submit" class="btn btn-secondary btn-sm btn-close"><i class="fa fa-times"></i></button></form>';
+    // El elemento que está despues de form se va ocultar
     $(this).after(boxComent).hide();
-    $('.add-coment').hide();
-
-    $('#text-coment').keyup(ajustarTextArea);
+    // Cuando escriba el areaTexto se va ajustar para que no salga el scroll
+    $('#text-coment').keyup(adjustTextArea);
+    // Cuando hace click en el ícono close
     $('.btn-close').click(function() {
       $('#form-comment').hide();
       $('.add-coment').show();
     });
+    // Cuando hace click en el check
     $('.btn-check').click(addComentTextArea);
-    // $('#form-comment').show();
-
   }
 
-
-
-  function ajustarTextArea(event) {
-    // console.log(event.target.scrollHeight);
+  function adjustTextArea(event) {
     event.target.style.height = '1px';
     event.target.style.height = (20 + event.target.scrollHeight) + 'px';
   }
 
-  function search() {
-    if ($(this).val()) {
-      // console.log($(this).val());
-      $('.card-place>div').each(function(index) {
-        // console.log(arrPlace)
-        // console.log(arrPlace[index])
-        // console.log($(this).children)
-        console.log($('.card-title'));
-        // console.log($('.card-body h6').text());
-        // if (!arrPlace[index].tags.match($('.card-body h6').text())) {
-        //   $(this).hide();
-        // }
+  // Validar el input buscador
+  function searchPlace() {
+    if ($(this).val()) { // dato que se ingresa en el input
+      var searchPlace = $(this).val(); // en la variable searchPlace se almacena el valor del input
+      $('.card-place>div').each(function(index) { // hace un recorrido con el each todos los div de los card-place
+        if ((arrPlace[index]).toUpperCase() === searchPlace.toUpperCase()) { // Comparacion del arraPlace con el valor ingresado
+          $('.card-place div').hide(); // ocultatar todos los paneles de la lista
+          $(this).show();// mostrar solo lo que cumple con la condicion
+        }
       });
     } else {
-      $('.card-place div').show();
+      $('.card-place div').show(); // mostrar todos las listas
     }
   }
-
-  // ajustarTextArea();
-
-  $('.card-place a').click(showDetailsPlace);
-  $('.add-coment').click(addComent);
-  $('#search-input').on('keyup', search);
-
-  // textTweet.addEventListener('keyup', ajustarTextArea);
+  // Llamando a la funcion para que cargue los lugares
+  showPlaces();
+  // eventos
+  $('.card-place a').click(showDetailsPlace); // cuando se hace click al nombre del lugar mostrar los detalles
+  $('.add-coment').click(addComentForm); // cuando se hace click en (Añadir comentarios...) se muestra el formulario
+  $('#search-input').on('keyup', searchPlace); // cada vez que inserto un valor en el input que me busque el lugar para mostrarlo
 }
-//
-// $(window).on('load', function() {
-//
-//   showPlaces();
-//
-//
-//   var dataPlace = getPlace();
-//   var arrPlace = Object.keys(dataPlace);
-//   console.log(arrPlace);
-//   $('#search-input').on('keyup', search);
-//   // Filtrando la seleccion
-//   function search() {
-//     if ($(this).val()) {
-//       // console.log($(this).val());
-//       $('.card-place>div').each(function(index) {
-//         console.log(arrPlace)
-//         // console.log(arrPlace[index])
-//         // if (!arrPlace[index].tags.match($('div.card>h6 ').val())) {
-//         //   $(this).hide();
-//         // }
-//       });
-//     } else {
-//       $('.card-place div').show();
-//     }
-//   }
-// })
+
 $(document).ready(begin);
