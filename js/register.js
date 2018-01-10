cf0ebb6c7eb654729b6fbf55351c7ad80642ddf6
jymmak <henryfrancisco_fernando@hotmail.com>
2018-01-10 10:46:58 -0500

    modificando firebase  registrar

--- js/register.js
+++ js/register.js
@@ -1,7 +1,8 @@
+
 function registrar() {
   var email = document.getElementById('email').value;
   var pass = document.getElementById('pass').value;
- 
+
   firebase.auth().createUserWithEmailAndPassword(email, pass).catch(function (error) {
     // Handle Errors here.
     var errorCode = error.code;
