9d81a747f3da3ae75bb55d0ec622ff60fcbb4b89
mebipeos <mebipeos@outlook.com>
2018-01-10 09:06:57 -0500

    Modificando la ventana del chat

--- js/register.js
+++ js/register.js
@@ -1,11 +1,15 @@
 function registrar() {
   var email = document.getElementById('email').value;
   var pass = document.getElementById('pass').value;
- 
+
   firebase.auth().createUserWithEmailAndPassword(email, pass).catch(function (error) {
     // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
     // ...
   });
+}
+
+function validar(){
+  
 }
