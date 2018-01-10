c741fc1ce9a7e6c47277a47fac4b910d4cef0503
jymmak <henryfrancisco_fernando@hotmail.com>
2018-01-10 01:28:12 -0500

    firebase register

--- js/app.js
+++ js/app.js
@@ -1,15 +1,6 @@
 $(document).ready(function () {
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyDh8a6XaxaCeFh9tatmoNMjB3Xh44A8q5s",
-    authDomain: "trip-lovers.firebaseapp.com",
-    databaseURL: "https://trip-lovers.firebaseio.com",
-    projectId: "trip-lovers",
-    storageBucket: "",
-    messagingSenderId: "234184574226"
-  };
 
-  $('.next, #send2').on('click', function (event) {
+  $('#send2').on('click', function (event) {
     event.preventDefault();
     window.location.href = 'views/search-places.html';
   });
@@ -25,25 +16,18 @@ $(document).ready(function () {
         e.preventDefault();
       });
   });
-  $('#phone').keyup(function () {
-    if ($(this).val().length > 9) {
-      $('.next').removeAttr('disabled');
-    } else {
-      $('.next').attr('disabled', 'disabled');
-    }
-  }).trigger('keyup');
+
 
   $('#send2').on('click', function (registrar) {
-    firebase.auth().createUserWithEmailAndPassword('#email2', '#pass2').catch(function(error) {
+    firebase.auth().createUserWithEmailAndPassword('#email2', '#pass2').catch(function (error) {
       // Handle Errors here.
       var errorCode = error.code;
       var errorMessage = error.message;
       // ...
     });
-    
- });  
-  });
 
+  });
+});
 
 
 
---
+++ js/register.js
@@ -0,0 +1,11 @@
+function registrar() {
+  var email = document.getElementById('email').value;
+  var pass = document.getElementById('pass').value;
+ 
+  firebase.auth().createUserWithEmailAndPassword(email, pass).catch(function (error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+  });
+}
