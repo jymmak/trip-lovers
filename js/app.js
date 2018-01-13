8502faed3b9c1af1474549a5dd0a90ac6b88259b
jymmak <henryfrancisco_fernando@hotmail.com>
2018-01-13 00:44:10 -0500

    mejora de formularios

--- js/app.js
+++ js/app.js
@@ -1,44 +1,81 @@
 $(document).ready(function () {
   // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyDh8a6XaxaCeFh9tatmoNMjB3Xh44A8q5s",
-    authDomain: "trip-lovers.firebaseapp.com",
-    databaseURL: "https://trip-lovers.firebaseio.com",
-    projectId: "trip-lovers",
-    storageBucket: "trip-lovers.appspot.com",
-    messagingSenderId: "234184574226"
+    apiKey: 'AIzaSyDh8a6XaxaCeFh9tatmoNMjB3Xh44A8q5s',
+    authDomain: 'trip-lovers.firebaseapp.com',
+    databaseURL: 'https://trip-lovers.firebaseio.com',
+    projectId: 'trip-lovers',
+    storageBucket: 'trip-lovers.appspot.com',
+    messagingSenderId: '234184574226'
   };
   firebase.initializeApp(config);
-  $('#send2').on('click', function (event) {
-    event.preventDefault();
-    window.location.href = 'views/search-places.html';
-  });
 
+  function Registrar() {
+    var $email = $('.email-res').val();
+    var $password = $('.pass-res').val();
 
-  $(function () {
-    $('#phone').keyup(function (e) {
-      if (this.value != '')
-        while (isNaN(this.value))
-          this.value = this.value.split('').reverse().join('').replace(/[\D]/i, '')
-            .split('').reverse().join('');
-    })
-      .on("cut copy paste", function (e) {
-        e.preventDefault();
+    firebase.auth().createUserWithEmailAndPassword($email, $password)
+      .then(function () {
+        writeUserData(uid, name);
+        $('.modal').modal();
+      })
+      .catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorCode);
       });
-  });
+  }
 
+  function Ingresar() {
+    var $email2 = $('.email-ingreso').val();
+    var $password2 = $('.pass-ingreso').val();
+    var $message = $('.message-error');
 
-  /*$('#send2').on('click', function () {
-    firebase.auth().createUserWithEmailAndPassword('#email2', '#pass2').catch(function (error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // ...
+    firebase.auth().signInWithEmailAndPassword($email2, $password2)
+      .then(function () {
+        next1();
+      })
+      .catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+        $message.append('<p class="red-text">*Datos incorretos</p>');
+      });
+  }
+
+  function Observador() {
+    firebase.auth().onAuthStateChanged(function (user) {
+      if (user) {
+        // User is signed in.
+        console.log('existes');
+
+        var displayName = user.displayName;
+        var email = user.email;
+        var emailVerified = user.emailVerified;
+        var photoURL = user.photoURL;
+        var isAnonymous = user.isAnonymous;
+        var uid = user.uid;
+        var providerData = user.providerData;
+        // ...
+      } else {
+        // User is signed out.
+        // ...
+
+        console.log('no existes');
+      }
     });
+  }
 
-  });*/
+  Observador();
+
+  function next1() {
+    setTimeout(function () {
+      window.location.href = 'home.html';
+    }, 500);
+  }
 });
-
 
 
 
--- js/login.js
+++ js/login.js
@@ -1,69 +1,46 @@
 $(document).ready(function () {
 
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyDh8a6XaxaCeFh9tatmoNMjB3Xh44A8q5s",
-    authDomain: "trip-lovers.firebaseapp.com",
-    databaseURL: "https://trip-lovers.firebaseio.com",
-    projectId: "trip-lovers",
-    storageBucket: "trip-lovers.appspot.com",
-    messagingSenderId: "234184574226"
-  };
-  firebase.initializeApp(config);
+  $('.contenedor-formularios').find('input, textarea').on('keyup blur focus', function (e) {
 
-  function entrar() {
-    var email2 = document.getElementById('email2').value;
-    var pass2 = document.getElementById('pass2').value;
+    var $this = $(this),
+      label = $this.prev('label');
 
-    firebase.auth().signInWithEmailAndPassword(email2, pass2).catch(function (error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // ...
-      document.getElementById('email2').value = "";
-      document.getElementById('pass2').value = "";
-
-    });
-  }
-
-  function observador() {
-    firebase.auth().onAuthStateChanged(function (user) {
-      if (user) {
-        console.log('existe un usuario activo');
-        aparece();
-        // User is signed in.
-        var displayName = user.displayName;
-
-        var email = user.email;
-        console.log(email);
-        var emailVerified = user.emailVerified;
-        var photoURL = user.photoURL;
-        var isAnonymous = user.isAnonymous;
-        var uid = user.uid;
-        var providerData = user.providerData;
-        // ...
+    if (e.type === 'keyup') {
+      if ($this.val() === '') {
+        label.removeClass('active highlight');
       } else {
-        // User is signed out.
-        console.log('no existe un usuario activo');
-
-        // ...
+        label.addClass('active highlight');
       }
-    });
-
-  }
-  console.log(observador());
-
-  function aparece() {
-    /*
-    var contenido = document.getElementById('contenido');
-    contenido.innerHTML = "Solo lo ve usuario activo";
-    //window.location.href='../views/news.html';
-    window.location.href='news.html';
-  }
-  
-  $(document).ready(function(){
-      entrar();
-      
+    } else if (e.type === 'blur') {
+      if ($this.val() === '') {
+        label.removeClass('active highlight');
+      } else {
+        label.removeClass('highlight');
+      }
+    } else if (e.type === 'focus') {
+      if ($this.val() === '') {
+        label.removeClass('highlight');
+      }
+      else if ($this.val() !== '') {
+        label.addClass('highlight');
+      }
     }
-  )*/
+
+  });
+
+  $('.tab a').on('click', function (e) {
+
+    e.preventDefault();
+
+    $(this).parent().addClass('active');
+    $(this).parent().siblings().removeClass('active');
+
+    target = $(this).attr('href');
+
+    $('.contenido-tab > div').not(target).hide();
+
+    $(target).fadeIn(300);
+
+  });
+
 });
