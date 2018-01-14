0b3a6f0ecdc2c1355c3c668451dd2e96fc53d3cd
jymmak <henryfrancisco_fernando@hotmail.com>
2018-01-14 03:21:47 -0500

    modificando js

--- js/splash.js
+++ js/splash.js
@@ -1,6 +1,6 @@
 /* Vista splash de index a sesion en 5 segundos */
 $(document).ready(function() {
   setTimeout(function() { 
-    window.location.href = 'views/search-places.html';
+    window.location.href = 'views/sesion.html';
   }, 3000);
 });
