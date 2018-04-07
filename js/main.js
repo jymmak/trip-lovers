c98fe75d63cd3093e1477349837fea9281100e8d
Jymma Karina Mogollon Cardoza <henryfrancisco_fernando@hotmail.com>
2018-04-07 03:06:30 -0500

    arreglo facebook

--- js/main.js
+++ js/main.js
@@ -32,11 +32,11 @@ function login(uid, name, email) {
 
 function signInFacebook() {
   var provider = new firebase.auth.FacebookAuthProvider();
-  firebase.auth().signInWithPopup(provider).then(function (result) {
+  firebase.auth().signInWithPopup(provider).then(function(result) {
     user = result.user;
     console.log(user);
     initApp();
-  }).catch(function (error) {
+  }).catch(function(error) {
     var errorCode = error.code;
     console.log(errorcode);
     var errorMessage = errorMessage;
@@ -49,29 +49,11 @@ function signInFacebook() {
 }
 
 function signInGoogle() {
-  if (!firebase.auth().currentUser) {
-    var provider = new firebase.auth.GoogleAuthProvider();
-    provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
-    firebase.auth().signInWithPopup(provider).then(function (result) {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      var token = result.credential.accessToken;
-      // The signed-in user info.
-      var user = result.user;
-      // ...
-    }).catch(function (error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // The email of the user's account used.
-      var email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      var credential = error.credential;
-      // ...
-      if (errorCode === 'auth/account-exists-with-different-credential') {
-        alert('Es el mismo usuario');
-      }
-    });
-  } else {
-    firebase.auth().signOut();
-  }
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    user = result.user;
+    console.log(user);
+    initApp();
+    window.location.href = '../views/search-places.html';
+  });
 }
