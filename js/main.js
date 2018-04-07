6c7ac4224100ee0ea13ffeadc0e21a1568885bab
Jymma Karina Mogollon Cardoza <henryfrancisco_fernando@hotmail.com>
2018-04-07 02:57:09 -0500

    accediendo a  facebook

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ $inigoogle.on('click', signInGoogle);
 function initApp() {
   registrationUsers(user.uid, user.displayName, user.email, user.photoURL);
   login(user.uid, user.displayName, user.email);
-  window.location.href = 'views/news.html';
+  window.location.href = '../views/search-places.html';
 }
 function registrationUsers(uid, name, email, photoURL) {
   firebase.database().ref('Usuarios/' + uid).set({
@@ -29,24 +29,14 @@ function login(uid, name, email) {
     email: email
   });
 }
-function signOut() {
-  firebase.auth().onAuthStateChanged(function(user) {
-    database.ref('/connected/' + user.uid).remove();
-  });
-  firebase.auth().signOut()
-    .then(function(result) {
-      console.log('Te has desconectado correctamente');
 
-      window.location.href = '../index.html';
-    });
-};
 function signInFacebook() {
   var provider = new firebase.auth.FacebookAuthProvider();
-  firebase.auth().signInWithPopup(provider).then(function(result) {
+  firebase.auth().signInWithPopup(provider).then(function (result) {
     user = result.user;
     console.log(user);
     initApp();
-  }).catch(function(error) {
+  }).catch(function (error) {
     var errorCode = error.code;
     console.log(errorcode);
     var errorMessage = errorMessage;
@@ -59,11 +49,29 @@ function signInFacebook() {
 }
 
 function signInGoogle() {
-  var provider = new firebase.auth.GoogleAuthProvider();
-  firebase.auth().signInWithPopup(provider).then(function(result) {
-    user = result.user;
-    console.log(user);
-    initApp();
-    window.location.href = 'views/news.html';
-  });
+  if (!firebase.auth().currentUser) {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+    firebase.auth().signInWithPopup(provider).then(function (result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      // ...
+    }).catch(function (error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+      if (errorCode === 'auth/account-exists-with-different-credential') {
+        alert('Es el mismo usuario');
+      }
+    });
+  } else {
+    firebase.auth().signOut();
+  }
 }
