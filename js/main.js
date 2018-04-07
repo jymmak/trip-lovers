97699064827c73cd7404fcccd63df146f7b47ab6
Jymma Karina Mogollon Cardoza <henryfrancisco_fernando@hotmail.com>
2018-04-07 12:53:49 -0500

    mejora inicio  sesió google

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ var $loginGoogle = $('#google-login');
 var $logout = $('.logout');
 $logout.on('click', signOut);
 $inifacebook.on('click', signInFacebook);
-
+$inigoogle.on('click', signInGoogle);
 function initApp() {
   registrationUsers(user.uid, user.displayName, user.email, user.photoURL);
   login(user.uid, user.displayName, user.email);
@@ -59,53 +59,25 @@ function signInFacebook() {
 }
 
 
-
-// Login con Google
-var providerGoogle = new firebase.auth.GoogleAuthProvider();
-$loginGoogle.click(function () {
-  firebase.auth().signInWithPopup(providerGoogle).then(function (result) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    // The signed-in user info.
+function signInGoogle() {
+  var provider = new firebase.auth.GoogleAuthProvider();
+  // Using a redirect.
+  firebase.auth().getRedirectResult().then(function (result) {
+    if (result.credential) {
+      // This gives you a Google Access Token.
+      var token = result.credential.accessToken;
+    }
     var user = result.user;
-    firebase.database().ref('users/' + user.uid).set({
-      name: user.displayName,
-      email: user.email,
-      uid: user.uid,
-      profilePhoto: user.photoURL
-    }).then(
-      user => {
-        console.log('Sesión con google');
-        $(location).attr('href', 'search-places.html');
-      });
-    // ...
-  }).catch(function (error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
-  });
-});
-
-// Obteniendo datos del usuario actual
-firebase.auth().onAuthStateChanged(function (user) {
-  if (user) {
-    // User is signed in.
-    var name = user.displayName;
-    var email = user.email;
-    var photoUrl = user.photoURL;
-    var emailVerified = user.emailVerified;
-    var uid = user.uid;
     console.log(user);
+    initApp();
+    window.location.href = '../views/search-places.html';
+    // 
+  });
 
-    $displayUsername.text(name);
-    // $userEmail.text(email);
-    $profilePhoto.attr('src', photoUrl);
-  } else {
-    // No user is signed in.
-  }
-});
+  // Start a sign in process for an unauthenticated user.
+  var provider = new firebase.auth.GoogleAuthProvider();
+  provider.addScope('profile');
+  provider.addScope('email');
+  firebase.auth().signInWithRedirect(provider);
+
+}
